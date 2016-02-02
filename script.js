1d1105a81cec4734acba2678a77200ac5d471ee7
Andrea Rendine <AndySky21@users.noreply.github.com>
2016-02-02 01:23:46 +0100

    Complete update
    * groupMissing is now set on-the-fly without the getter
    * fixed the list of elements/type whose validityState is aligned
    * insertElements,insertFieldset,insertForms fixed: now they accept any number of params, with better efficiency.
    * init optimized. Now it can be used to generically re-init areas of massive change in the page.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 var requiredAlternative = {
+	n: 'groupMissing',
 	req: [
-		'fieldset',
 		'text','search','url','tel','email','password','datetime','date','month','week','time','number','checkbox','file',
 		'textarea',
 		'select-one','select-multi'
@@ -32,20 +32,20 @@ var requiredAlternative = {
 		}
 		return msg;
 	},
-	cycle: function(callback1,callback2,haystack,storeVar){
-		var list = haystack.querySelectorAll('fieldset, input, textarea, select, keygen, output, button');
+	cycle: function(callback,extend,haystack,storeVar){
+		var list = haystack.querySelectorAll('button, fieldset, input, keygen, output, object, select, textarea');
 		// element.validity will be fixed on all elements supporting it. They call it consistency.
 		var result = 0;
 		var elm;
 		for(var i = 0; i < list.length; i++){
 			elm = list[i];
 			if(requiredAlternative.req.indexOf(elm.type) > -1){
-				result += this[callback1](elm);
+				result += this[callback](elm);
 				if(storeVar !== undefined){
 					storeVar.push(elm);
 				}
-			} else if(callback2){
-				this[callback2](elm);
+			} else if(extend){
+				this[callback](elm);
 			}
 		}
 		return result;
@@ -53,9 +53,7 @@ var requiredAlternative = {
 	check: function(elm){
 		return (elm.type == 'checkbox') ? elm.checked : Boolean(elm.value);
 	},
-	getGroup: function(node){
-		var result = false;
-		var stack = [];
+	getFieldset: function(node){
 		var fieldset;
 		do{
 			if(node.tagName == 'FIELDSET'){
@@ -64,8 +62,13 @@ var requiredAlternative = {
 			}
 			node = node.parentElement;
 		} while(node.tagName != 'FORM' && node.tagName != 'BODY');
+		return fieldset;
+	},
+	getGroup: function(fieldset){
+		var result = false;
 		if(fieldset){
-			var required = fieldset.dataset.required || fieldset.getAttribute('required') || null;
+			var stack = [];
+			var required = fieldset.dataset.required || fieldset.getAttribute('required');
 			if(required !== null){
 				var filled = this.cycle('check',false,fieldset,stack)
 				required = (required == '' || isNaN(required)) ? stack.length : Math.min(stack.length, parseInt(required));
@@ -74,26 +77,28 @@ var requiredAlternative = {
 		}
 		return result;
 	},
-	action: function(elm,added){
-		var init = (elm.tagName == 'FIELDSET');
-		var result = this.getGroup(elm);
+	action: function(node,k){
+		var fieldset = (node.tagName == 'FIELDSET') ? node : this.getFieldset(node);
+		var result = this.getGroup(fieldset);
 		if(result){
 			var newMsg = this.setMsg(result.required,result.stack.length);
-			var oldMsg = (added) ? this.setMsg(result.required, result.stack.length - 1) : newMsg;
-			if(result.filled < result.required){
-				for(var z = 0; z < result.stack.length; z++){
+			var oldMsg = (k) ? this.setMsg(result.required, result.stack.length - k) : newMsg;
+			// at this point allow for error definition in parent fieldset, too
+			result.stack.push(fieldset);
+			var valid = (result.filled == result.required);
+			var elm;
+			for(var z = 0; z < result.stack.length; z++){
+				elm = result.stack[z];
+				if(!elm.validity.customError || elm.validationMessage == oldMsg){
 					// sets groupMissing error. Also removes it from controls which have been filled in
-					if(!result.stack[z].validity.customError || result.stack[z].validationMessage == oldMsg){
-						result.stack[z].setCustomValidity(this.check(result.stack[z]) ? '' : newMsg);
+					if(!valid && !this.check(elm)){
+						// this loop will not be done on error messages initialisation
+						elm.setCustomValidity(newMsg);
+					} else {
+						elm.setCustomValidity('');
 					}
 				}
-			} else if(!init){
-				// this loop will not be done on error messages initialisation
-				for(var z = 0; z < result.stack.length; z++){
-					if(result.stack[z].validationMessage == oldMsg){
-						result.stack[z].setCustomValidity('');
-					}
-				}
+				elm.validity[this.n] = !valid;
 			}
 		}
 	},
@@ -103,35 +108,20 @@ var requiredAlternative = {
 			requiredAlternative.action(this);
 		});
 	},
-	setProperty: function(elm,simple){
-		var n = 'groupMissing'
+	setProperty: function(elm){
 		Object.defineProperty(elm, 'validity', {
 			// prevents UAs from reassigning 'validity' on Invalid event
 			value: {}
 		});
-		if(simple){
-			Object.defineProperty(elm.validity, n, {
-				// this is just for consistency (!)
-				enumerable: true,
-				writable: true,
-				value: false
-			});
-		} else {
-			Object.defineProperty(elm.validity, 'element', {
-				// workaround not to define Validity object from scratch
-				value: elm
-			});
-			Object.defineProperty(elm.validity, n, {
-				// value will be missing if all elements in the group are empty/unchecked and fieldset.required is truthy
-				enumerable: true,
-				get: function(){
-					var result = requiredAlternative.getGroup(this.element);
-					return (result) ? (result.filled < result.required) : false;
-				}
-			});
-		}
+		Object.defineProperty(elm.validity, this.n, {
+			// extends validity state for all elements implementing it
+			enumerable: true,
+			writable: true,
+			value: false
+		});
 		Object.defineProperty(elm.validity, 'valid', {
 			// validity according to new groupMissing
+			enumerable: true,
 			get: function(){
 				var result = true;
 				for(var prop in this){
@@ -148,41 +138,71 @@ var requiredAlternative = {
 		this.setEvent(elm);
 		this.setProperty(elm);
 	},
-	defaultProperty: function(elm){
-		this.setProperty(elm, true);
-	},
 	setFieldset: function(fieldset){
 		Object.defineProperty(fieldset, 'required', {
 			// required in fieldset returns the minimum number of required fields
+			enumerable: true,
 			get: function(){
 				var result = requiredAlternative.getGroup(fieldset);
 				return (result) ? result.required : false;
 			}
 		});
-		this.action(fieldset);
 	},
-	initFieldsets: function(haystack){
-		var list = haystack.getElementsByTagName('FIELDSET');
+	initElements: function(list){
+		var add;
+		if(list.tagName){
+			this.cycle('setElement',true,list);
+			list = list.getElementsByTagName('FIELDSET');
+		} else {
+			add = true;
+		}
 		for(var i = 0; i < list.length; i++){
+			if(add){
+				this.cycle('setElement',true,list[i]);
+			}
 			this.setFieldset(list[i]);
+			this.action(list[i]);
 		}
 	},
+	/* These methods are to be invoked in case a new control or a new form are inserted into the document */
 	init: function(haystack){
 		if(haystack === undefined){
 			haystack = document.body;
 			this.msg = requiredAlternative.message();
 		}
-		this.cycle('setElement','defaultProperty',haystack);
-		this.initFieldsets(haystack);
+		this.initElements(haystack);
 	},
-	/* These methods are to be invoked in case a new control or a new form are inserted into the document */
-	insertElement: function(elm){
+	insertElements: function(){
 		// if an element has been added, the message to match is computed differently
-		this.setElement(elm);
-		this.action(elm,true);
+		var sets = {
+			'el': [],
+			'n': []
+		};
+		var elm,fieldset,k;
+		for(var i in arguments){
+			elm = arguments[i];
+			this.setElement(elm);
+			fieldset = this.getFieldset(elm);
+			k = sets['el'].indexOf(fieldset);
+			if(k == -1){
+				sets['el'].push(fieldset);
+				sets['n'].push(1);
+			} else {
+				sets['n'][k] += 1;
+			}
+		}
+		for(var i = 0;i < sets.el.length;i++){
+			console.log(sets.el[i] +','+ sets.n[i]);
+			this.action(sets.el[i],sets.n[i]);
+		}
 	},
-	insertForm: function(form){
-		this.init(form);
+	insertFieldsets: function(){
+		this.initElements(arguments);
+	},
+	insertForms: function(){
+		for(var i in arguments){
+			this.init(arguments[i]);
+		}
 	}
 }
 document.addEventListener('DOMContentLoaded', function(done) {
